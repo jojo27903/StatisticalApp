685085f1aeee1bef268c089710ed3947134901fc
Azizah Jois Pradani <75002234+jojo27903@users.noreply.github.com>
2023-06-13 10:46:22 +0700

    Update REGRESSION.py

--- pages/REGRESSION.py
+++ pages/REGRESSION.py
@@ -1,21 +1,23 @@
 import streamlit as st
-import pandas as pd
-import numpy as np
-from sklearn.linear_model import LinearRegression
 
 def perform_linear_regression(x, y):
-    # Create a linear regression model
-    model = LinearRegression()
+    # Calculate the regression coefficients
+    n = len(x)
+    x_mean = sum(x) / n
+    y_mean = sum(y) / n
 
-    # Fit the model to the data
-    model.fit(x, y)
+    numerator = sum((x[i] - x_mean) * (y[i] - y_mean) for i in range(n))
+    denominator = sum((x[i] - x_mean) ** 2 for i in range(n))
 
-    return model
+    slope = numerator / denominator
+    intercept = y_mean - slope * x_mean
+
+    return intercept, slope
 
 def main():
     # Set title and description
     st.title("Simple Linear Regression")
-    st.markdown('This app show you intercept and slope.')
+    st.markdown('This app shows the intercept and slope.')
     st.write("Perform simple linear regression.")
 
     # Get data from the user
@@ -26,19 +28,14 @@ def main():
     x = [float(row[0]) for row in data_list]
     y = [float(row[1]) for row in data_list]
 
-    # Convert the lists to numpy arrays
-    x = np.array(x).reshape(-1, 1)
-    y = np.array(y)
-
     # Perform linear regression
     if st.button("Perform Linear Regression"):
-        model = perform_linear_regression(x, y)
+        intercept, slope = perform_linear_regression(x, y)
 
         # Display the regression results
         st.write("Regression Coefficients:")
-        st.write(f"Intercept: {model.intercept_:.4f}")
-        st.write(f"Slope: {model.coef_[0]:.4f}")
-
+        st.write(f"Intercept: {intercept:.4f}")
+        st.write(f"Slope: {slope:.4f}")
 
 # Run the app
 if __name__ == "__main__":
